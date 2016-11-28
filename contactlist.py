90194d6f26c33450da97aa379a00b91c387f87e7
amaramalik356 <amara.malik@gmail.com>
2016-11-28 15:51:53 -0800

    updated version 2

--- contactlist.py
+++ contactlist.py
@@ -1,22 +1,38 @@
 
 def main():
-	process_file_to_list
+	contacts = process_file_to_list()
+	exit = False
+	while not exit:	
+		firstname = raw_input('What name are you looking for?').lower() # "Rose".lower() # eventually this will be raw_input
+		print find_name_in_list(firstname, contacts)
 
 def process_file_to_list():
 	contacts = open ("contactlist.txt","r")
 	contact_list = contacts.readlines()
 	contacts.close()
-	firstname=raw_input ("Enter First Name")
-	for line in contact_list:
-		if record [0] == firstname:
-			firstname = True
-		else:
-			firstname = False
-	if firstname == False:
-		print ("Name not on contact list")
-	else:
-		print "record[0]\n,record[1]\n,record[2]\n,record[3]\n" in contact_list
+	return contact_list
 
+def find_name_in_list (firstname,contact_list):
+	for contact in contact_list:
+		striped_contact = contact.strip()
+		record = striped_contact.split("|")
+		# print striped_contact, record
+		if record[0].lower() == firstname:
+			return record
+
+
+
+# firstname=raw_input ("Enter First Name")
+	# for line in contact_list:
+	# 	if record [0] == firstname:
+	# 		firstname = True
+	# 	else:
+	# 		firstname = False
+	# if firstname == False:
+	# 	print ("Name not on contact list")
+	# else:
+	# 	print "record[0]\n,record[1]\n,record[2]\n,record[3]\n" in contact_list
+	# print contact_list
 
 if __name__ == '__main__':
 	main()
@@ -37,7 +53,7 @@ if __name__ == '__main__':
 # else:
 # 	print "record[0]\n,record[1]\n,record[2]\n,record[3]\n" in my_file
 
-contactlist.close()
+# contactlist.close()
 
 # if __name__ == '__main__':
 # 	main()
